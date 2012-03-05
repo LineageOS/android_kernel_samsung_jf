1ee18a3a722b0e72ae399bacb332dab76987e583
Tejun Heo <tj@kernel.org>
2012-03-05 13:15:24 -0800

    block: interface update for ioc/icq creation functions
    Make the following interface updates to prepare for future ioc related
    changes.
    
    * create_io_context() returning ioc only works for %current because it
      doesn't increment ref on the ioc.  Drop @task parameter from it and
      always assume %current.
    
    * Make create_io_context_slowpath() return 0 or -errno and rename it
      to create_task_io_context().
    
    * Make ioc_create_icq() take @ioc as parameter instead of assuming
      that of %current.  The caller, get_request(), is updated to create
      ioc explicitly and then pass it into ioc_create_icq().
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Change-Id: I23baafc30820d2d4bc0021e2c60227e77a5e1151

--- block/blk-core.c
+++ block/blk-core.c
@@ -824,7 +824,7 @@ retry:
 			 */
 			if (!ioc && !retried) {
 				spin_unlock_irq(q->queue_lock);
-				create_io_context(current, gfp_mask, q->node);
+				create_io_context(gfp_mask, q->node);
 				spin_lock_irq(q->queue_lock);
 				retried = true;
 				goto retry;
@@ -889,7 +889,9 @@ retry:
 
 	/* create icq if missing */
 	if ((rw_flags & REQ_ELVPRIV) && unlikely(et->icq_cache && !icq)) {
-		icq = ioc_create_icq(q, gfp_mask);
+		ioc = create_io_context(gfp_mask, q->node);
+		if (ioc)
+			icq = ioc_create_icq(ioc, q, gfp_mask);
 		if (!icq)
 			goto fail_icq;
 	}
@@ -977,7 +979,7 @@ static struct request *get_request_wait(struct request_queue *q, int rw_flags,
 		 * up to a big batch of them for a small period time.
 		 * See ioc_batching, ioc_set_batching
 		 */
-		create_io_context(current, GFP_NOIO, q->node);
+		create_io_context(GFP_NOIO, q->node);
 		ioc_set_batching(q, current->io_context);
 
 		spin_lock_irq(q->queue_lock);
