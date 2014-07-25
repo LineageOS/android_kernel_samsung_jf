f4a838f75b2cf2377f601aad371e569953188f2c
Stephen Boyd <sboyd@codeaurora.org>
2014-07-25 11:26:45 -0700

    hwrng: Pass entropy to add_hwgenerator_randomness() in bits, not bytes
    The entropy argument to add_hwgenerator_randomness() is passed
    directly to credit_entropy_bits() so we should be passing the
    number of bits, not bytes.
    
    Change-Id: I068c1073245944dddf0cbf7e69f1c0f1d96010e2
    Fixes: 646d838eef81 "hwrng: create filler thread"
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/char/hw_random/core.c
+++ drivers/char/hw_random/core.c
@@ -334,7 +334,7 @@ static int hwrng_fillfn(void *unused)
 			continue;
 		}
 		add_hwgenerator_randomness((void *)rng_fillbuf, rc,
-					   (rc*current_quality)>>10);
+					   rc * current_quality * 8 >> 10);
 	}
 	hwrng_fill = 0;
 	return 0;
