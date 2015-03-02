89339696e5c7e6ce18e4585054588dbf73756ebc
Lorenzo Colitti <lorenzo@google.com>
2015-03-02 12:06:45 +0900

    Set the iif for IPv6 packets as well.
    This was fixed upstream in a more comprehensive way by 1fb9489bf,
    but this one-line fix is consistent with the rest of the 3.4
    networking code.
    
    Change-Id: I342ca90c6d0213a7aeac9a2b18283530998f81d7
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -961,6 +961,8 @@ struct dst_entry * ip6_route_output(struct net *net, const struct sock *sk,
 {
 	int flags = 0;
 
+	fl6->flowi6_iif = LOOPBACK_IFINDEX;
+
 	if ((sk && sk->sk_bound_dev_if) || rt6_need_strict(&fl6->daddr))
 		flags |= RT6_LOOKUP_F_IFACE;
 
