d0ae95836af2989addf9cc362b8f16b852085bbe
Binoy Jayan <bjayan@codeaurora.org>
2016-02-19 09:27:14 +0530

    video: msm: Fix NULL pointer dereferences and trivial issues
    Fixed the following NULL pointer dereferences and
    a few trivial issues.
    
    Pointer '__p' returned from call to function 'kmalloc' at line 3968
    may be NULL and will be dereferenced at line 3970.
    drivers/video/msm/mdp.c +3970 | mdp_probe()
    
    Pointer '__p' returned from call to function 'kmalloc' at line 3969
    may be NULL and will be dereferenced at line 3971.
    drivers/video/msm/mdp.c +3971 | mdp_probe()
    
    Suspicious dereference of pointer 'mfd' before NULL check at line 487.
    drivers/video/msm/mdp_dma.c +486 | mdp_dma2_update()
    
    Pointer 'mfd' checked for NULL at line 487 will be dereferenced
    at line 512.
    drivers/video/msm/mdp_dma.c +512 | mdp_dma2_update()
    
    Suspicious dereference of pointer 'mfd' before NULL check at line 139.
    drivers/video/msm/mdp_dma_s.c +138 | mdp_dma_s_update()
    
    Pointer 'mfd' checked for NULL at line 139 will be dereferenced
    at line 165.
    drivers/video/msm/mdp_dma_s.c +165 | mdp_dma_s_update()
    
    Pointer 'mipi_dsi_pdata' checked for NULL at line 403 may be dereferenced
    at line 441.
    drivers/video/msm/mipi_dsi.c +441 | mipi_dsi_probe()
    
    Pointer 'mipi_dsi_pdata' checked for NULL at line 423 may be dereferenced
    at line 441.
    drivers/video/msm/mipi_dsi.c +441 | mipi_dsi_probe()
    
    Pointer 'cmds_tx' returned from call to function 'kmalloc' at line 1233
    may be NULL and may be dereferenced at line 1241.
    drivers/video/msm/mipi_dsi_host.c +1241 | mipi_dsi_cmds_single_tx()
    
    Pointer 'mipi_novatek_pdata' checked for NULL at line 468 will be
    dereferenced at line 474.
    drivers/video/msm/mipi_novatek.c +474 | mipi_novatek_set_backlight()
    
    Array 'pmctl­->pcam_ptr-­>dev_inst_map' of size 18
    may use index value(s) 0..18
    drivers/media/video/msm/msm_isp.c +383 | msm_isp_notify_vfe()
    drivers/media/video/msm/msm_mctl_buf.c +364
    
    Change-Id: I7ce6f72ad1d77d0cab16271716cbc3d85b81d380
    Signed-off-by: Binoy Jayan <bjayan@codeaurora.org>

--- drivers/video/msm/mdp_dma_s.c
+++ drivers/video/msm/mdp_dma_s.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2009, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2009, 2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -135,8 +135,11 @@ static void mdp_dma_s_update_lcd(struct msm_fb_data_type *mfd)
 
 void mdp_dma_s_update(struct msm_fb_data_type *mfd)
 {
+	if (!mfd || !mfd->dma)
+		return;
+
 	down(&mfd->dma->mutex);
-	if ((mfd) && (!mfd->dma->busy) && (!mdp_fb_is_power_off(mfd))) {
+	if (!mfd->dma->busy && !mdp_fb_is_power_off(mfd)) {
 		down(&mfd->sem);
 		mdp_enable_irq(MDP_DMA_S_TERM);
 		if (mfd->panel_info.type == MDDI_PANEL)
