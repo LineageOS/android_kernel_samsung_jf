21e1ddb00146d0a67f0034d1811cd7ad14c86f01
Oleg Nesterov <oleg@redhat.com>
2016-08-02 14:03:19 -0700

    proc_oom_score: remove tasklist_lock and pid_alive()
    This was needed before to ensure that ->signal != 0 and do_each_thread()
    is safe, see commit b95c35e76b29b ("oom: fix the unsafe usage of
    badness() in proc_oom_score()") for details.
    
    Today tsk->signal can't go away and for_each_thread(tsk) is always safe.
    
    Link: http://lkml.kernel.org/r/20160608211921.GA15508@redhat.com
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Tetsuo Handa <penguin-kernel@i-love.sakura.ne.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: Ic669203b6cecf26eaea68778d463a921cfea03de

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -414,11 +414,8 @@ static int proc_oom_score(struct task_struct *task, char *buffer)
 	unsigned long totalpages = totalram_pages + total_swap_pages;
 	unsigned long points = 0;
 
-	read_lock(&tasklist_lock);
-	if (pid_alive(task))
-		points = oom_badness(task, NULL, NULL, totalpages) *
-						1000 / totalpages;
-	read_unlock(&tasklist_lock);
+	points = oom_badness(task, NULL, NULL, totalpages) *
+					1000 / totalpages;
 	return sprintf(buffer, "%lu\n", points);
 }
 
