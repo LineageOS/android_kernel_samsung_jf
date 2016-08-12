f57be330450d48d5dc72dfedd29ee2df6bcaa45f
Javi Ferrer <javi.f.o@gmail.com>
2016-08-12 22:04:51 +0200

    battery: max77693: Be more AOSP-friendly
    Change-Id: I8e8b183d1305d5d88fef2720c4014c5ea3f34be1

--- drivers/battery/max77693_charger.c
+++ drivers/battery/max77693_charger.c
@@ -705,10 +705,14 @@ static int sec_chg_get_property(struct power_supply *psy,
 		val->intval = max77693_get_health_state(charger);
 		break;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
-		val->intval = charger->charging_current_max;
+		// AOSP expects the charging current to be in microamperes
+		// frameworks/base/core/java/android/os/BatteryManager.java L256
+		val->intval = charger->charging_current_max * 1000;
 		break;
 	case POWER_SUPPLY_PROP_CURRENT_AVG:
-		val->intval = charger->charging_current;
+		// AOSP expects the charging current to be in microamperes
+		// frameworks/base/core/java/android/os/BatteryManager.java L263
+		val->intval = charger->charging_current * 1000;
 		break;
 	case POWER_SUPPLY_PROP_CURRENT_NOW:
 		val->intval = max77693_get_input_current(charger);
