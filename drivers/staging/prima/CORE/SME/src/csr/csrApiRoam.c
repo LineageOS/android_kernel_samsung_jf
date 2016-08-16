de3c5b2dcf650b05868160e88ef5aafc4e4bf8aa
SaidiReddy Yenuga <saidir@codeaurora.org>
2016-08-16 10:50:09 +0530

    wlan: Increase the Wait for key timer value
    qcacld-2.0 to prima propagation
    
    The wait for key timer value in the driver should
    be greater than that of supplicant.
    
    Hence increasing it to 15 seconds.
    
    CRs-Fixed: 919403
    Change-Id: I5d6e8d445ab9dcef73b923595f41564e0a6e3c6e

--- drivers/staging/prima/CORE/SME/src/csr/csrApiRoam.c
+++ drivers/staging/prima/CORE/SME/src/csr/csrApiRoam.c
@@ -94,8 +94,10 @@
 #define CSR_DEF_IBSS_START_CHANNEL_50       36
 #define CSR_DEF_IBSS_START_CHANNEL_24       1
 #define CSR_IBSS_JOIN_TIMEOUT_PERIOD        ( 1 *  PAL_TIMER_TO_SEC_UNIT )  // 1 second
-#define CSR_WAIT_FOR_KEY_TIMEOUT_PERIOD         ( 5 * PAL_TIMER_TO_SEC_UNIT )  // 5 seconds, for WPA, WPA2, CCKM
-#define CSR_WAIT_FOR_WPS_KEY_TIMEOUT_PERIOD         ( 120 * PAL_TIMER_TO_SEC_UNIT )  // 120 seconds, for WPS
+/* 15 seconds, for WPA, WPA2, CCKM */
+#define CSR_WAIT_FOR_KEY_TIMEOUT_PERIOD     (15 * PAL_TIMER_TO_SEC_UNIT)
+/* 120 seconds, for WPS */
+#define CSR_WAIT_FOR_WPS_KEY_TIMEOUT_PERIOD (120 * PAL_TIMER_TO_SEC_UNIT)
 /*---------------------------------------------------------------------------
   OBIWAN recommends [8 10]% : pick 9% 
 ---------------------------------------------------------------------------*/
