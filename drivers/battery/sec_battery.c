83453afd3530390262eaf2bfe279605e9b864aed
Dave Daynard <nardholio@gmail.com>
2017-05-19 07:31:54 +0200

    sec_battery: Migrate to alarmtimer interface
    Change-Id: Idbd78413af1f47ee044df136290c5d220a5f4878

--- drivers/battery/sec_battery.c
+++ drivers/battery/sec_battery.c
@@ -129,15 +129,13 @@ static int sec_bat_set_charge(
 				bool enable)
 {
 	union power_supply_propval val;
-	ktime_t current_time;
 	struct timespec ts;
 
 	val.intval = battery->status;
 	psy_do_property("sec-charger", set,
 		POWER_SUPPLY_PROP_STATUS, val);
 
-	current_time = alarm_get_elapsed_realtime();
-	ts = ktime_to_timespec(current_time);
+	get_monotonic_boottime(&ts);
 
 	if (enable) {
 		val.intval = battery->cable_type;
@@ -936,23 +934,28 @@ static void  sec_bat_event_program_alarm(
 	struct sec_battery_info *battery, int seconds)
 {
 	ktime_t low_interval = ktime_set(seconds - 10, 0);
-	ktime_t slack = ktime_set(20, 0);
 	ktime_t next;
 
 	next = ktime_add(battery->last_event_time, low_interval);
-	alarm_start_range(&battery->event_termination_alarm,
-		next, ktime_add(next, slack));
+	/* The original slack time called for, 20 seconds, exceeds
+	 * the length allowed for an unsigned long in nanoseconds. Use
+	 * ULONG_MAX instead
+	 */
+	hrtimer_start_range_ns(&battery->event_termination_hrtimer,
+		next, ULONG_MAX, HRTIMER_MODE_ABS);
 }
 
-static void sec_bat_event_expired_timer_func(struct alarm *alarm)
+enum hrtimer_restart sec_bat_event_expired_timer_func(struct hrtimer *timer)
 {
 	struct sec_battery_info *battery =
-		container_of(alarm, struct sec_battery_info,
-			event_termination_alarm);
+		container_of(timer, struct sec_battery_info,
+			event_termination_hrtimer);
 
 	battery->event &= (~battery->event_wait);
 	dev_info(battery->dev,
 		"%s: event expired (0x%x)\n", __func__, battery->event);
+
+	return HRTIMER_NORESTART;
 }
 
 static void sec_bat_event_set(
@@ -971,7 +974,7 @@ static void sec_bat_event_set(
 		return;
 	}
 
-	alarm_cancel(&battery->event_termination_alarm);
+	hrtimer_cancel(&battery->event_termination_hrtimer);
 	battery->event &= (~battery->event_wait);
 
 	if (enable) {
@@ -987,7 +990,7 @@ static void sec_bat_event_set(
 			return;	/* nothing to clear */
 		}
 		battery->event_wait = event;
-		battery->last_event_time = alarm_get_elapsed_realtime();
+		battery->last_event_time = ktime_get_boottime();
 
 		sec_bat_event_program_alarm(battery,
 			battery->pdata->event_waiting_time);
@@ -1116,11 +1119,9 @@ static bool sec_bat_time_management(
 				struct sec_battery_info *battery)
 {
 	unsigned long charging_time;
-	ktime_t	current_time;
 	struct timespec ts;
 
-	current_time = alarm_get_elapsed_realtime();
-	ts = ktime_to_timespec(current_time);
+	get_monotonic_boottime(&ts);
 
 	if (battery->charging_start_time == 0) {
 		dev_dbg(battery->dev,
@@ -1616,18 +1617,21 @@ static void sec_bat_program_alarm(
 				struct sec_battery_info *battery, int seconds)
 {
 	ktime_t low_interval = ktime_set(seconds, 0);
-	ktime_t slack = ktime_set(10, 0);
 	ktime_t next;
 
 	next = ktime_add(battery->last_poll_time, low_interval);
-	alarm_start_range(&battery->polling_alarm,
-		next, ktime_add(next, slack));
+	/* The original slack time called for, 10 seconds, exceeds
+	 * the length allowed for an unsigned long in nanoseconds. Use
+	 * ULONG_MAX instead
+	 */
+	hrtimer_start_range_ns(&battery->polling_hrtimer,
+		next, ULONG_MAX, HRTIMER_MODE_ABS);
 }
 
-static void sec_bat_alarm(struct alarm *alarm)
+enum hrtimer_restart sec_bat_alarm(struct hrtimer *timer)
 {
-	struct sec_battery_info *battery = container_of(alarm,
-				struct sec_battery_info, polling_alarm);
+	struct sec_battery_info *battery = container_of(timer,
+				struct sec_battery_info, polling_hrtimer);
 
 	/* In wake up, monitor work will be queued in complete function
 	 * To avoid duplicated queuing of monitor work,
@@ -1638,6 +1642,8 @@ static void sec_bat_alarm(struct alarm *alarm)
 		queue_work(battery->monitor_wqueue, &battery->monitor_work);
 		dev_dbg(battery->dev, "%s: Activated\n", __func__);
 	}
+
+	return HRTIMER_NORESTART;
 }
 
 
@@ -1774,7 +1780,7 @@ static void sec_bat_set_polling(
 				polling_time_temp * HZ);
 		break;
 	case SEC_BATTERY_MONITOR_ALARM:
-		battery->last_poll_time = alarm_get_elapsed_realtime();
+		battery->last_poll_time = ktime_get_boottime();
 		if (battery->pdata->monitor_initial_count) {
 			battery->pdata->monitor_initial_count--;
 			sec_bat_program_alarm(battery, 1);
@@ -1801,7 +1807,7 @@ static void sec_bat_monitor_work(
 
 	dev_dbg(battery->dev, "%s: Start\n", __func__);
 
-	c_ts = ktime_to_timespec(alarm_get_elapsed_realtime());
+	c_ts = ktime_to_timespec(ktime_get_boottime());
 
 	/* monitor once after wakeup */
 	if (battery->polling_in_sleep) {
@@ -3032,9 +3038,11 @@ static int __devinit sec_battery_probe(struct platform_device *pdev)
 #endif
 	battery->ps_enable= 0;
 
-	alarm_init(&battery->event_termination_alarm,
-			ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP,
-			sec_bat_event_expired_timer_func);
+	hrtimer_init(&battery->event_termination_hrtimer,
+			CLOCK_BOOTTIME,
+			HRTIMER_MODE_ABS);
+	battery->event_termination_hrtimer.function =
+			&sec_bat_event_expired_timer_func;
 
 	battery->temp_high_threshold =
 		pdata->temp_high_threshold_normal;
@@ -3099,10 +3107,12 @@ static int __devinit sec_battery_probe(struct platform_device *pdev)
 			sec_bat_polling_work);
 		break;
 	case SEC_BATTERY_MONITOR_ALARM:
-		battery->last_poll_time = alarm_get_elapsed_realtime();
-		alarm_init(&battery->polling_alarm,
-			ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP,
-			sec_bat_alarm);
+		battery->last_poll_time = ktime_get_boottime();
+		hrtimer_init(&battery->polling_hrtimer,
+			CLOCK_BOOTTIME,
+			HRTIMER_MODE_ABS);
+		battery->polling_hrtimer.function =
+			&sec_bat_alarm;
 		break;
 	default:
 		break;
@@ -3229,13 +3239,13 @@ static int __devexit sec_battery_remove(struct platform_device *pdev)
 		cancel_delayed_work(&battery->polling_work);
 		break;
 	case SEC_BATTERY_MONITOR_ALARM:
-		alarm_cancel(&battery->polling_alarm);
+		hrtimer_cancel(&battery->polling_hrtimer);
 		break;
 	default:
 		break;
 	}
 
-	alarm_cancel(&battery->event_termination_alarm);
+	hrtimer_cancel(&battery->event_termination_hrtimer);
 	flush_workqueue(battery->monitor_wqueue);
 	destroy_workqueue(battery->monitor_wqueue);
 	wake_lock_destroy(&battery->monitor_wake_lock);
@@ -3269,7 +3279,7 @@ static int sec_battery_prepare(struct device *dev)
 		cancel_delayed_work(&battery->polling_work);
 		break;
 	case SEC_BATTERY_MONITOR_ALARM:
-		alarm_cancel(&battery->polling_alarm);
+		hrtimer_cancel(&battery->polling_hrtimer);
 		break;
 	default:
 		break;
@@ -3312,7 +3322,7 @@ static void sec_battery_complete(struct device *dev)
 
 	/* cancel current alarm and reset after monitor work */
 	if (battery->pdata->polling_type == SEC_BATTERY_MONITOR_ALARM)
-		alarm_cancel(&battery->polling_alarm);
+		hrtimer_cancel(&battery->polling_hrtimer);
 
 	wake_lock(&battery->monitor_wake_lock);
 	queue_work(battery->monitor_wqueue,
