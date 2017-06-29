39d5be775c7ac408f5028f61ee38e2dbc60e5704
Mukul Sharma <mukul@codeaurora.org>
2017-06-29 18:25:39 +0530

    wlan: Exit BMPS during wlan resume
    Wlan firmware locks interrupt during exit & enter BMPS.
    During wlan resume, host sends multiple commands to firmware and
    While processing of each smd commands, it exit/enter bmps mode.
    Because of much interrupt locking, BT thread get delayed and user
    experiences glitches during  a2dp streaming.
    
    As a part of this fix, Host exit BMP during starting of resume.
    Later once resume gets complete, host enter again in bmps.
    
    Change-Id: I00dac523755bcb1c3ade407cb2840a9060e23ed6
    CRs-Fixed: 2069225

--- drivers/staging/prima/CORE/HDD/inc/wlan_hdd_main.h
+++ drivers/staging/prima/CORE/HDD/inc/wlan_hdd_main.h
@@ -114,7 +114,7 @@
 /** Maximum time(ms)to wait for disconnect to complete **/
 #define WLAN_WAIT_TIME_DISCONNECT  500
 #define WLAN_WAIT_TIME_STATS       800
-#define WLAN_WAIT_TIME_POWER       800
+#define WLAN_WAIT_TIME_POWER       5000
 #define WLAN_WAIT_TIME_COUNTRY     1000
 #define WLAN_WAIT_TIME_CHANNEL_UPDATE   600
 
