8519d0010e46dff7cc263d09f746f949f92dac56
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2018-07-11 17:51:13 +0530

    prima: Fix possible buffer overflow in sir_convert_addts_rsp2_struct
    In the function sir_convert_addts_rsp2_struct, iterator j is
    assigned with the value pAddTs->numTclas + addts.num_WMMTCLAS.
    The j value is used as the index to the array pAddTs->tclasInfo.
    Maximum limit on  pAddTs->tclasInfo entries is 2. So when the
    value of j exceeds 2, then a possible buffer overflow could
    occur.
    
    Validate the value of j against SIR_MAC_TCLASIE_MAXNUM(2).
    
    Change-Id: Icc723380ed4ccd51c729194d509e288be0e0712c
    CRs-Fixed: 2264429

--- drivers/staging/prima/CORE/SYS/legacy/src/utils/src/parserApi.c
+++ drivers/staging/prima/CORE/SYS/legacy/src/utils/src/parserApi.c
@@ -3269,7 +3269,7 @@ sirConvertAddtsReq2Struct(tpAniSirGlobal    pMac,
         if ( addts.num_WMMTCLAS )
         {
             j = (tANI_U8)(pAddTs->numTclas + addts.num_WMMTCLAS);
-            if ( SIR_MAC_TCLASIE_MAXNUM > j ) j = SIR_MAC_TCLASIE_MAXNUM;
+            if ( SIR_MAC_TCLASIE_MAXNUM < j ) j = SIR_MAC_TCLASIE_MAXNUM;
 
             for ( i = pAddTs->numTclas; i < j; ++i )
             {
@@ -3451,7 +3451,7 @@ sirConvertAddtsRsp2Struct(tpAniSirGlobal    pMac,
         if ( addts.num_WMMTCLAS )
         {
             j = (tANI_U8)(pAddTs->numTclas + addts.num_WMMTCLAS);
-            if ( SIR_MAC_TCLASIE_MAXNUM > j ) j = SIR_MAC_TCLASIE_MAXNUM;
+            if ( SIR_MAC_TCLASIE_MAXNUM < j ) j = SIR_MAC_TCLASIE_MAXNUM;
 
             for ( i = pAddTs->numTclas; i < j; ++i )
             {
